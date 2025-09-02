b5dcc98a53859f82a0baa65410622400fc5d8e2d
Javacoke <tlsrjs0227@naver.com>
2025-09-02 17:06:49 +0900

    ADD: team work

---
+++ java_advanced/src/workshop1/StudentGradeCalculator.java
@@ -0,0 +1,31 @@
+package workshop1;
+
+
+//
+public class Student_Grade_Calculator {
+
+Student_Grade_Calculator.calculate(student);
+
+
+
+
+
+}
+//    int koren ;
+//    int english;
+//    int math;
+//    int science;
+//
+//    public static void calculate {
+//
+//        }
+//
+//        private static String getGradeByAverage(double avg) {
+//            if (avg >= 90) return "A";
+//            else if (avg >= 80) return "B";
+//            else if (avg >= 70) return "C";
+//            else if (avg >= 60) return "D";
+//            else return "F";
+//        }
+//    }
+
